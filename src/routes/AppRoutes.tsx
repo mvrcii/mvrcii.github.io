5026660fc079804fd9d9fab04491ad22105cadf7
Marcel Roth <MarcelRoth100@aol.com>
2024-10-07 20:42:47 +0200

    Move Slot component to AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,26 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {Slot} from "../components/Slot";
+import {BrowserRouter, Outlet, Route, Routes} from 'react-router-dom';
 import {MainContent} from "../components/main/MainContent";
 import {ErrorPage} from "../components/misc/ErrorPage.tsx";
+import {styled} from "@mui/material/styles";
+import {Box} from "@mui/material";
+
+export function Slot() {
+    return (
+        <StyledBox component='main' id='main'>
+            <Outlet/>
+        </StyledBox>
+    );
+}
+
+const StyledBox = styled(Box)({
+    flexGrow: 1,  // Allows main to expand and fill space
+    flex: '1 0 auto',
+    overflow: 'auto',  // Allows scrolling within the main content
+    marginTop: '2rem',
+    // Offset for scrollIntoView navigation to account for sticky header
+    // 2rem for main´s top margin, 6.25rem for header height, 4rem for header bottom margin
+    scrollMargin: 'calc(2rem + 6.25rem + 4rem)'
+});
 
 export function AppRoutes() {
     return (
